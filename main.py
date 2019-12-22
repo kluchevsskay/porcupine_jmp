26110e6b4740561aa1fa27714d0843d81d4dec18
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2019-12-22 21:05:03 +0300

    1) фиксация камеры на будущем персонаже 2) изменение изображений

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ from pygame.locals import *
 import sys
 import random
 
+FPS = 50
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
 
 class DoodleJump:
 
     def __init__(self):
-
         # создание экрана, спрайтов и шрифта
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((1000, 1000))
         pygame.font.init()
         self.score = 0
         self.font = pygame.font.SysFont("Arial", 25)
@@ -23,3 +41,20 @@ class DoodleJump:
         self.playerLeft_1 = pygame.image.load("data/Melissa_Fall2_L.png").convert_alpha()
         self.spring = pygame.image.load("data/spring.png").convert_alpha()
         self.spring_1 = pygame.image.load("data/spring_1.png").convert_alpha()
+
+
+    def event(self, event):
+        """обработка событий"""
+        camera = Camera()
+
+        # изменяем ракурс камеры
+        camera.update(player)
+
+        # обновляем положение всех спрайтов
+        for sprite in all_sprites:
+            camera.apply(sprite)
+        if event.type == QUIT:
+            sys.exit()
+        # elif event.type == KEYUP:
+        # if event.key == K_ESCAPE:
+        # что-то делается
